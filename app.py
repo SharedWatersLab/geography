19300bcefe8ce872c9920133847bfe9501a3bf7e
Selena <selena.wallace@tufts.edu>
2025-01-16 16:22:47 -0500

    paths updates

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ if st.button("Start Download"):
     if uname:
         paths, current_user = get_user(basin, uname, "pdf")
         with st.spinner("Downloading..."):
-            full_process(current_user)
+            full_process(current_user, paths)
         st.success("Download completed successfully!")
     else:
         st.error("Please enter a username.") 
